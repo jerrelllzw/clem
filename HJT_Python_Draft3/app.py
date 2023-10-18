c55b90aa83243f7fe10d78568a8126c0c60189a6
Jerrell Low <69338396+jerrelllzw@users.noreply.github.com>
2023-10-18 10:08:24 +0800

    Add files via upload

---
+++ HJT_Python_Draft3/app.py
@@ -0,0 +1,131 @@
+from flask import Flask, render_template, request, jsonify,redirect, url_for
+from searchEngine import (
+    load_data, 
+    search_by_year, 
+    search_by_name, 
+    search_by_author_lastname, 
+    search_by_author_firstname, 
+    search_by_journal, 
+    search_by_title, 
+    search_by_keyword,
+    search_by_paper_id
+)
+import json
+
+HTML_FILE = 'index.html'
+DATABASE_NAME = './master.json'
+KEY_FULLNAME = 'fullname'
+KEY_FIRSTNAME = 'firstname'
+KEY_LASTNAME = 'lastname'
+KEY_YEAR = 'year'
+KEY_JOURNAL = 'journal'
+KEY_JOURNAL_ARTICLE_TITLE = 'journalarticletitle'
+KEY_KEYWORD = 'keyword'
+KEY_PAPER_ID = 'paper_id'
+
+
+# To start webapp
+app = Flask(__name__)
+
+# web_app_url/<route>
+
+@app.route('/')
+def index():
+    return render_template(HTML_FILE)
+
+# submit button sends a http post request to web_app_url/search. This function the handles the request
+@app.route('/search', methods=['POST'])
+def search():
+    search_data = request.json
+    dataBase = load_data(DATABASE_NAME)
+    matching_publications = []
+    #matching_publications = dataBase  # Start with all publications
+    print(search_data)
+    
+    isAndSearch = True if search_data.get('searchType') == 'and' else False
+    
+    if isAndSearch:
+        matching_publications = dataBase  # Start with all publications
+        for key, value in search_data.items():
+            if value == '':
+                continue
+            elif key == KEY_FULLNAME:
+                matching_publications = [pub for pub in matching_publications if pub in search_by_name(dataBase, value)]
+            elif key == KEY_FIRSTNAME:
+                matching_publications = [pub for pub in matching_publications if pub in search_by_author_firstname(dataBase, value)]
+            elif key == KEY_LASTNAME:
+                matching_publications = [pub for pub in matching_publications if pub in search_by_author_lastname(dataBase, value)]
+            elif key == KEY_YEAR:
+                matching_publications = [pub for pub in matching_publications if pub in search_by_year(dataBase, value)]
+            elif key == KEY_JOURNAL:
+                matching_publications = [pub for pub in matching_publications if pub in search_by_journal(dataBase, value)]
+            elif key == KEY_JOURNAL_ARTICLE_TITLE:
+                matching_publications = [pub for pub in matching_publications if pub in search_by_title(dataBase, value)]
+            elif key == KEY_KEYWORD:
+                matching_publications = [pub for pub in matching_publications if pub in search_by_keyword(dataBase, value)]
+            elif key == KEY_PAPER_ID:  
+                matching_publications = [pub for pub in matching_publications if pub in search_by_paper_id(dataBase, value)]
+    else:
+        for key in search_data.keys():
+            if search_data[key] == '':
+                continue
+            elif key == KEY_FULLNAME:
+                matching_publications += search_by_name(dataBase, search_data[key])
+            elif key == KEY_FIRSTNAME:
+                matching_publications += search_by_author_firstname(dataBase, search_data[key])
+            elif key == KEY_LASTNAME:
+                matching_publications += search_by_author_lastname(dataBase, search_data[key])
+            elif key == KEY_YEAR:
+                matching_publications += search_by_year(dataBase, search_data[key])
+            elif key == KEY_JOURNAL:
+                matching_publications += search_by_journal(dataBase, search_data[key])
+            elif key == KEY_JOURNAL_ARTICLE_TITLE:
+                matching_publications += search_by_title(dataBase, search_data[key])
+            elif key == KEY_KEYWORD:
+                matching_publications += search_by_keyword(dataBase, search_data[key])
+            elif key == KEY_PAPER_ID:  
+                matching_publications += search_by_paper_id(dataBase, search_data[key])
+        matching_publications = removeDuplicates(matching_publications)
+    
+    return jsonify(matching_publications)
+
+def removeDuplicates(publications):
+    seen = set()
+    unique_publications = []
+    for publication in publications:
+        pub_id = publication.get('paper_id', '')
+        if pub_id not in seen:
+            unique_publications.append(publication)
+            seen.add(pub_id)
+    return unique_publications
+
+@app.route('/advanced_search')
+def advanced_search():
+    return render_template('advanced_search.html')
+
+@app.route('/advanced_search/results', methods=['POST'])
+def advanced_search_results():
+    search_data = request.json
+    dataBase = load_data(DATABASE_NAME)
+    year = search_data.get('year')
+    condition = search_data.get('condition')  
+    year = int(year)
+    if condition == "before":
+        matching_publications = [pub for pub in dataBase if int(pub[KEY_YEAR]) < year]
+    elif condition == "after":
+        matching_publications = [pub for pub in dataBase if int(pub[KEY_YEAR]) > year]
+    elif condition == "equals":
+        matching_publications = [pub for pub in dataBase if int(pub[KEY_YEAR]) == year]
+    elif condition == "before_or_equals":
+        matching_publications = [pub for pub in dataBase if int(pub[KEY_YEAR]) <= year]
+    elif condition == "after_or_equals":
+        matching_publications = [pub for pub in dataBase if int(pub[KEY_YEAR]) >= year]
+    else:
+        print(matching_publications)
+        return jsonify({"error": "Invalid search condition"})
+    #print(matching_publications)
+    return jsonify(matching_publications)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
+
---
+++ HJT_Python_Draft3/searchEngine.py
@@ -0,0 +1,127 @@
+import json
+
+# Define json fields
+FIELD_PAPER_ID = 'paper_id'
+FIELD_AUTHORS = 'authors'
+SUBFIELD_AUTHORS_FNAME = 'fname'
+SUBFIELD_AUTHORS_LNAME = 'lname'
+SUBFIELD_AUTHORS_CAUTHOR = 'cauthor'
+SUBFIELD_AUTHORS_EMAIL = 'email'
+FIELD_YEAR = 'year'
+FIELD_PAPER = 'paper'
+SUBFIELD_PAPER_TITLE = 'title'
+SUBFIELD_PAPER_KEYWORDS = 'keywords'
+FIELD_SOURCE = 'source'
+SUBFIELD_SOURCE_JOURNAL = 'journal'
+SUBFIELD_SOURCE_VOLUME = 'volume'
+SUBFIELD_SOURCE_NUMBER = 'number'
+SUBFIELD_SOURCE_PAGES = 'pages'
+SUBFIELD_SOURCE_PAGES_START = 's_page'
+SUBFIELD_SOURCE_PAGES_END = 'e_page'
+
+# Load the data
+def load_data(filename):
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            return data
+    except FileNotFoundError:
+        print("File not found")
+        return None
+    except json.decoder.JSONDecodeError:
+        print("Invalid JSON")
+        return None
+    except Exception as e:
+        print("Unknown error:", str(e))
+        return None
+
+
+def search_by_year(data, year):
+    return [publication for publication in data if publication[FIELD_YEAR] == int(year)]
+
+def search_by_name(data, partial_names):
+    partial_names = set(partial_names.lower().split())
+    return [publication for publication in data for author in publication[FIELD_AUTHORS] 
+            if partial_names.intersection(author[SUBFIELD_AUTHORS_FNAME].lower().split() + author[SUBFIELD_AUTHORS_LNAME].lower().split())]
+
+def search_by_author_lastname(data, lastNames):
+    lastNames = set(lastNames.lower().split())
+    return [publication for publication in data for author in publication[FIELD_AUTHORS] 
+            if lastNames.intersection(author[SUBFIELD_AUTHORS_LNAME].lower().split())]
+
+def search_by_author_firstname(data, firstNames):
+    firstNames = set(firstNames.lower().split())
+    return [publication for publication in data for author in publication[FIELD_AUTHORS] 
+            if firstNames.intersection(author[SUBFIELD_AUTHORS_FNAME].lower().split())]
+
+def search_by_journal(data, journal_words):
+    return [publication for publication in data if journal_words.lower() in publication[FIELD_SOURCE][SUBFIELD_SOURCE_JOURNAL].lower()]
+
+def search_by_title(data, title_words):
+    return [publication for publication in data if title_words.lower() in publication[FIELD_PAPER][SUBFIELD_PAPER_TITLE].lower()]
+
+def search_by_keyword(data, keyword_words):
+    keyword_words = keyword_words.lower().split()
+    matching_publications = []
+
+    for publication in data:
+        keyword_list = [k.lower() for k in publication[FIELD_PAPER][SUBFIELD_PAPER_KEYWORDS]]
+        for word in keyword_words:
+            if any(word in k for k in keyword_list):
+                matching_publications.append(publication)
+                break  
+
+    return matching_publications
+
+def search_by_paper_id(data, paper_id):
+    results = []
+    for publication in data:
+        if str(publication.get('paper_id', '')) == str(paper_id):
+            results.append(publication)
+    return results
+
+'''
+#def search_by_keyword(data, keyword_words):
+    keyword_words = keyword_words.lower().split()
+    matching_publications = []
+
+    for publication in data:
+        keyword_list = [k.lower() for k in publication[FIELD_PAPER][SUBFIELD_PAPER_KEYWORDS]]
+        for word in keyword_words:
+            if any(word in k for k in keyword_list):
+                matching_publications.append(publication)
+                break  
+
+    return matching_publications
+
+#import json
+
+author_search1 = input("Please key in a first author last name to search for:")
+author_search2 = input("Please key in a second author last name to search for:")
+
+print ()
+
+f = open("master.json", encoding="utf8")
+
+publications = json.loads(f.read())
+
+i = 0
+
+for publication in publications:
+    author_list = []
+    for author in publication["authors"]:
+        author_list.append(author["lname"])
+    if author_search1 in author_list and author_search2 in author_list:                         # Boolean "AND"
+        print ("Paper ID:", publication ["paper_id"])
+        print (publication["paper"]["title"])
+        print ("\n")
+        i = i + 1
+
+print (f"\nAuthors with last name {author_search1} and {author_search2}, published {i} article(s) together.")
+            
+f.close()
+
+
+#Search by keywords
+#Search by number of pages
+#Search by two authors names'''
